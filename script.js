c986f9f34de5eba6fafb0b7b168108aed3aa4f8a
Ophelie <opheliewask@yahoo.fr>
2023-01-20 15:46:20 +0100

    done2 modification date

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('http://whatyearitit-backend.vercel.app/date')
+fetch('https://whatyearitit-backend.vercel.app/year')
  .then(response => response.json())
  .then(data => {
     document.querySelector('#year').textContent = data.year;
